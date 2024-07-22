8ed9204bbe0b08e081e711d9c585a9bcf70e84e5
Emil <emroyzkd@gmail.com>
2024-07-22 11:47:32 +0300

    first commit

--- src/components/TodoFooter/TodoFooter.jsx
+++ src/components/TodoFooter/TodoFooter.jsx
@@ -3,20 +3,27 @@ import TodoFooterList from "./TodoFooterList/TodoFooterList";
 import "./TodoFooter.css";
 import PropTypes from "prop-types";
 
-const TodoFooter = ({ category, selectCategory, deleteAllCompleted }) => {
+const TodoFooter = ({
+  category,
+  tasks,
+  selectCategory,
+  deleteAllCompleted,
+}) => {
   TodoFooter.defaultProps = {
     category: [],
+    tasks: [],
     selectCategory: () => {},
     deleteAllCompleted: () => {},
   };
   TodoFooter.propTypes = {
+    tasks: PropTypes.array,
     category: PropTypes.array,
     selectCategory: PropTypes.func,
     deleteAllCompleted: PropTypes.func,
   };
   return (
     <footer className="footer">
-      <span className="todo-count">1 items left</span>
+      <span className="todo-count">items left</span>
       <TodoFooterList selectCategory={selectCategory} category={category} />
       <button onClick={deleteAllCompleted} className="clear-completed">
         Clear completed
